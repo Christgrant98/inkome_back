55d4fa852c2f5e52bb873a4d1b3377b2c6c784b4
Christian <christian.garcerant@uac.edu.co>
2023-03-27 14:05:29 -0500

    Create the user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_10_192956) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_27_185939) do
   create_table "adverts", force: :cascade do |t|
     t.string "description"
     t.string "name"
     t.integer "age"
     t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "phone"
+    t.string "fullname"
+    t.integer "age"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
